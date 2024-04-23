97446d684e0343ebd290b8068e128e63e48640bd
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-23 20:27:56 +0000

    Update Linked List documentation

---
+++ dsa/solution_2.js
@@ -0,0 +1,16 @@
+// Detect Cycle in Linked List
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    
+    if (slow === fast) {
+      return true;
+    }
+  }
+  
+  return false;
+}
