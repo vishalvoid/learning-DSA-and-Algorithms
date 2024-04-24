a51e884dab16c5fb42a10a08103b5e18cbc8e0a0
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-24 21:21:24 +0000

    Add error handling to Array Operations

---
+++ dsa/solution_12.js
@@ -0,0 +1,16 @@
+// Dutch National Flag Algorithm
+function sortColors(nums) {
+  let low = 0, mid = 0, high = nums.length - 1;
+  
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++; mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    }
+  }
+}
