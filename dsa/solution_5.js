cd5033040b2d7f650640c11930505f84cc123da7
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-24 14:39:33 +0000

    Simplify Linked List logic

---
+++ dsa/solution_5.js
@@ -0,0 +1,19 @@
+// Merge Two Sorted Lists
+function mergeTwoLists(l1, l2) {
+  const dummy = new ListNode(0);
+  let current = dummy;
+  
+  while (l1 !== null && l2 !== null) {
+    if (l1.val <= l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+  
+  current.next = l1 || l2;
+  return dummy.next;
+}
