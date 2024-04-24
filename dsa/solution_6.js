eebb77c66e3e3dddf16f43bfc4a4ebbbcbad490b
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-24 17:22:44 +0000

    Improve Array Operations efficiency

---
+++ dsa/solution_6.js
@@ -0,0 +1,12 @@
+// Kadane's Algorithm - Maximum Subarray
+function maxSubArray(nums) {
+  let maxSoFar = nums[0];
+  let maxEndingHere = nums[0];
+  
+  for (let i = 1; i < nums.length; i++) {
+    maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+  }
+  
+  return maxSoFar;
+}
