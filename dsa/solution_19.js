b798fd0e2fe20971488f6e64ff1aaf7484d8e9ad
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-25 19:27:55 +0000

    Update Dynamic Programming algorithm

---
+++ dsa/solution_19.js
@@ -0,0 +1,8 @@
+// Fibonacci with Memoization
+function fibonacci(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  
+  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+  return memo[n];
+}
