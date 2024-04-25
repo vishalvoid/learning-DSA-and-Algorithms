a12366c07e43bb121ddd6d9a2587a04f8f8e0253
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-25 20:48:48 +0000

    Simplify Sorting Algorithms logic

---
+++ dsa/solution_26.js
@@ -0,0 +1,37 @@
+// Heap Sort
+function heapSort(arr) {
+  buildMaxHeap(arr);
+  
+  for (let i = arr.length - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, 0, i);
+  }
+  
+  return arr;
+}
+
+function buildMaxHeap(arr) {
+  const n = arr.length;
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, i, n);
+  }
+}
+
+function heapify(arr, i, n) {
+  let largest = i;
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+  
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+  
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+  
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, largest, n);
+  }
+}
