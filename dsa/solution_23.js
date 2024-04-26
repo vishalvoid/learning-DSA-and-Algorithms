bfac790e4f9d9c42fa5e7c84b7a2bbc0f918da5b
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-04-26 03:27:40 +0000

    Add edge case handling for Dynamic Programming

---
+++ dsa/solution_23.js
@@ -0,0 +1,24 @@
+// Edit Distance
+function editDistance(word1, word2) {
+  const m = word1.length, n = word2.length;
+  const dp = Array(m + 1).fill().map(() => Array(n + 1).fill(0));
+  
+  for (let i = 0; i <= m; i++) dp[i][0] = i;
+  for (let j = 0; j <= n; j++) dp[0][j] = j;
+  
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(
+          dp[i - 1][j] + 1,    // deletion
+          dp[i][j - 1] + 1,    // insertion
+          dp[i - 1][j - 1] + 1 // substitution
+        );
+      }
+    }
+  }
+  
+  return dp[m][n];
+}
